b09b04e630ee82112199169e5fc019e234924a54
Ahindra D <official.ahindra@gmail.com>
2024-12-21 03:43:04 +0530

    delete_note_instruction

--- anchor/programs/notesols/src/lib.rs
+++ anchor/programs/notesols/src/lib.rs
@@ -19,6 +19,8 @@ pub mod notesols {
         note_entry.owner = ctx.accounts.owner.key();
         note_entry.title = title;
         note_entry.message = message;
+        msg!("Created Note: {}", note_entry.title);
+        msg!("Message: {}", note_entry.message);
         Ok(())
     }
 
@@ -32,6 +34,12 @@ pub mod notesols {
         let note_entry = &mut ctx.accounts.note_entry;
         note_entry.owner = ctx.accounts.owner.key();
         note_entry.message = message;
+        msg!("Upadted! Message: {}", note_entry.message);
+        Ok(())
+    }
+
+    pub fn delete_note_entry(_ctx: Context<DeleteNote>, title: String) -> Result<()> {
+        msg!("Deleted Note: {}", title);
         Ok(())
     }
 }
@@ -76,6 +84,23 @@ pub struct UpdateNote<'info> {
     realloc=ANCHOR_DISCRIMINATOR+NoteEntryState::INIT_SPACE,
     realloc::zero=true,
     realloc::payer=owner,
+  )]
+    pub note_entry: Account<'info, NoteEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title:String)]
+pub struct DeleteNote<'info> {
+    #[account(
+    mut,
+    seeds=[title.as_bytes(),owner.key().as_ref()],
+    bump,
+    close=owner,
   )]
     pub note_entry: Account<'info, NoteEntryState>,
 
